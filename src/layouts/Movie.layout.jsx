cd95bac646788bb2aea6a8066f9369e1df76d31e
unknown <hujareshubham1781@gmail.com>
2021-09-23 01:59:48 +0530

    Movie + Plays Page

--- src/layouts/Movie.layout.jsx
+++ src/layouts/Movie.layout.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
+import MovieNavbar from '../components/Navbar/movieNavbar.component';
 
 //components
 import Navbar from "../components/Navbar/navbar.component";
 
 
+
 export const MovieLayout = (props) => {
     return (
         <>
-              <Navbar />
+              <MovieNavbar />
         
             {props.children}
         
